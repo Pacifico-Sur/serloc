0f394ea38466a7778af030f9c8587a31758528d7
davidmacer <jdavid.macer@gmail.com>
2023-07-13 18:12:13 -0600

    Agrega primera versión de app de Shiny para ejecutar el servicio
    Esta primera versión está tratando de ejecutar el tipo de consulta por localidad

---
+++ app.R
@@ -0,0 +1,183 @@
+library(shiny)
+library(DBI)
+library(magrittr)
+library(tibble)
+library(sf)
+library(dplyr)
+library(RPostgres)
+
+if (!require("ipa")) remotes::install_gitlab("davidmacer/ipa@develop")
+
+# Conecta con la base de datos del servicio de localidades
+conexion <- ipa::db_connect(dbname = "siclr_db",
+                            host = "database")
+
+### Define las variables de entrada para las listas desplegables
+tipo_consulta <- list("Selecciona una opción de consulta" = "null",
+                      "Localidades" = "id_loc",
+                      "Propiedad social" = "id_ps",
+                      "Propiedad privada" = "id_pp",
+                      "Municipio" = "id_mun")
+estados <- list("Selecciona un estado" = 0,
+                "Chiapas" = 7,
+                "Guerrero" = 12,
+                "Oaxaca" = 20)
+lista_temas <- list("Carencia de bienes y medios de comunicación" = 2,
+                    "Exclusión indígena" = 3,
+                    "Incidencia de analfabetismo" = 7,
+                    "Presencia de piso de tierra, carencia de agua y saneamiento" = 5,
+                    "Presencia de población económicamente inactiva" = 9,
+                    "Presencia de población en edad avanzada y población con discapacidad" = 8,
+                    "Presencia de población infantil y en edad dependiente" = 6,
+                    "Rezago educativo, carencia de auto e internet" = 4,
+                    "Aspectos cualitativos de vulnerabilidad" = 10)
+anio_consulta <- list("2010" = 2010,
+                      "2020" = 2020)
+###
+# Define la interfaz de usuario
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Servicio de Información y Conocimiento de 
+               Localidades Rurales y sus Territorios"),
+
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(
+        sidebarPanel(),
+        
+        # Show a plot of the generated distribution
+        mainPanel(
+          # El usuario escoge qué tipo de consulta quiere hacer
+          div(
+            selectInput(inputId = "id_tipo_consulta",
+                        label = "Tipo de consulta",
+                        choices = tipo_consulta,
+                        selected = NULL)
+          ),
+          # Debe escoger el estado de interés
+          div(
+            selectInput(inputId = "id_estado",
+                        label = "Estado",
+                        choices = estados)
+          ),
+          # El contenido de la lista de municipios se llena según el estado seleccionado
+          div(
+            selectInput(inputId = "id_municipio",
+                        label = "Municipio",
+                        choices = NULL)
+          ),
+          # El contenido de la lista de localidades se llena según el municipio seleccionado
+          div(
+            selectInput(inputId = "id_localidades",
+                        label = "Localidad",
+                        choices = NULL)
+          ),
+          # Temas de interés
+          div(
+            selectInput(inputId = "id_tema",
+                        label = "Tema",
+                        choices = lista_temas)
+          ),
+          # Año de interés
+          div(
+            selectInput(inputId = "id_anio",
+                        label = "Año de consulta",
+                        choices = anio_consulta)
+          ),
+          uiOutput(outputId = "subtemaInputUI"),
+          # Indicadores
+          uiOutput(outputId = "indicadorInputUI"),
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output, session) {
+  
+  ### Evento para llenar la lista de municipios según el estado seleccionado
+  observe({
+    id_estado <- input$id_estado
+    
+    # Extrae de la BD los municipios del estado seleccionado
+    query_municipios <- paste("SELECT * FROM edo_mun.municipios WHERE ",
+                              "\"ID_ENT\"", " = ", id_estado, ";")
+    municipios <- ipa::db_get_table(conn = conexion,
+                                    statement = query_municipios)
+    municipios <- municipios |>
+      as_tibble() |>
+      select(id_mun, nomgeo) |>
+      arrange(nomgeo)
+    
+    municipios <- setNames(as.list(municipios$id_mun), municipios$nomgeo)
+    primer_elemento <- list("Selecciona un municipio" = 0)
+    municipios <- primer_elemento |>
+      append(municipios)
+    
+    # Can also set the label and select items
+    updateSelectInput(session, "id_municipio",
+                      label = "Municipios",
+                      choices = municipios)
+  })
+  ###
+  
+  ### Evento para llenar la lista de localidades según el municipio seleccionado
+  observe({
+    req(input$id_municipio)
+    id_mun <- input$id_municipio
+    
+    # Extrae de la BD los municipios del estado seleccionado
+    query_localidades <- paste("SELECT * FROM loc.localidades WHERE ",
+                               "\"ID_MUN\"", " = ", id_mun, ";")
+    localidades <- ipa::db_get_table(conn = conexion,
+                                     statement = query_localidades)
+    localidades <- localidades |>
+      as_tibble() |>
+      select(id_loc, nom_loc) |>
+      arrange(nom_loc)
+    
+    localidades <- setNames(as.list(localidades$id_loc), localidades$nom_loc)
+    primer_elemento <- list("Localidad" = 0)
+    localidades <- primer_elemento |>
+      append(localidades)
+    
+    # Can also set the label and select items
+    updateSelectInput(session, "id_localidades",
+                      label = "Localidades",
+                      choices = localidades)
+  })
+  ###
+  
+  ### Muestra la lista de subtemas cuando selecciona Aspectos cualitativos de vulnerabilidad
+  output$subtemaInputUI <- renderUI({
+    if (input$id_tema == 10) {
+      query_subtemas <- "SELECT * FROM catalogo.des_soc_subtema;"
+      lista_subtemas <- ipa::db_get_table(conn = conexion,
+                                          statement = query_subtemas)
+      selectInput(inputId = "id_subtema",
+                  label = "Subtemas",
+                  choices = lista_subtemas$subtema)
+    }
+  })
+  ###
+  
+  ### Inicio evento para selecciones los indicadores de índice y nivel
+  output$indicadorInputUI <- renderUI({
+    id_tema <- input$id_tema
+    anio <- input$id_anio
+    query_indicadores <- paste0("SELECT * FROM catalogo.indicadores
+                          WHERE cve_sub = (SELECT cve_sub FROM catalogo.subtema
+                                          WHERE cve_tem = ", id_tema, " and 
+                                          anio = ", anio, ");")
+      tbl_indicadores <- ipa::db_get_table(conn = conexion,
+                                          statement = query_indicadores)
+      checkboxGroupInput(inputId = "id_indicadores",
+                         label = "Indicadores",
+                         choices = tbl_indicadores$indicadores,
+                         selected = 1)
+  })
+  ###
+  
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
